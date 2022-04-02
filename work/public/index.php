a9c2ff8a840f84d9666c80e978c97a348ca0747c
s-ike <shunt_i@yahoo.co.jp>
2022-04-02 16:44:34 +0900

    update files

--- work/public/index.php
+++ work/public/index.php
@@ -1,78 +1,9 @@
 <?php
-session_start();
-
-define('DSN', 'mysql:host=db;dbname=myapp;charset=utf8mb4');
-define('DB_USER', 'myappuser');
-define('DB_PASS', 'myapppass');
-define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
+require_once(__DIR__ . '/../app/config.php');
 
 createToken();
 
-try {
-    $pdo = new PDO(
-        DSN,
-        DB_USER,
-        DB_PASS,
-        [
-            // エラーが起きたとき、例外をスロー
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            // オブジェクト形式で取得
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-            // SQLで指定した型で取得
-            PDO::ATTR_EMULATE_PREPARES => false,
-        ]
-    );
-} catch (PDOException $e) {
-    echo $e->getMessage();
-    exit;
-}
-
-function h($str)
-{
-    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
-}
-
-function createToken()
-{
-    if (!isset($_SESSION['token'])) {
-        $_SESSION['token'] = bin2hex(random_bytes(32));
-    }
-}
-
-function validateToken()
-{
-    if (empty($_SESSION['token'])
-    || $_SESSION['token'] !== filter_input(INPUT_POST, 'token')) {
-        exit('Invalid post request');
-    }
-}
-
-function addTodo($pdo)
-{
-    $title = trim(filter_input(INPUT_POST, 'title'));
-    if ($title === '') {
-        return;
-    }
-
-    $stmt = $pdo->prepare("INSERT INTO todos (title) VALUES (:title)");
-    $stmt->bindValue('title', $title, PDO::PARAM_STR);
-    $stmt->execute();
-}
-
-function getTodos($pdo)
-{
-    $stmt = $pdo->query("SELECT * FROM todos ORDER BY id DESC");
-    $todos = $stmt->fetchAll();
-    return $todos;
-}
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    validateToken();
-    addTodo($pdo);
-
-    header('Location: ' . SITE_URL);
-    exit;
-}
+$pdo = getPdoInstance();
 
 $todos = getTodos($pdo);
 ?>
